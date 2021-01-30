a37209ca8d177a80e0ea14510c2f41b5915dc824
parulsurana <parulsurana28@gmail.com>
2021-01-30 10:23:28 +0530

    backend

---
+++ modules/Project.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = mongoose.Schema({
+    projectname: {
+      type:String,
+      required: true,
+    },
+    features: {
+      type:String,
+      required: true,
+    }
+  });
+
+  export default mongoose.model("Project", ProjectSchema) ;
